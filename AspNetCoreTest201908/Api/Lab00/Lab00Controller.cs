8c1ca9ec457146c2c5834c94c09d077c4bdc95df
Cash Wu <cash@cashwu.com>
2019-08-14 10:25:48 +0800

    :tada: Initial commit.

---
+++ AspNetCoreTest201908/Api/Lab00/Lab00Controller.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTest201908.Api.Lab00
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class Lab00Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok(new { IsSuccess = true });
+        }
+    }
+}
---
+++ AspNetCoreTest201908/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreTest201908.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
