faf0c26fd05fed865bc9fb90dc7ddc434d6a3622
Cash <cash@cashwu.com>
2019-08-19 09:30:20 +0800

    Lab01 test

--- UnitTests/Lab01Tests.cs
+++ UnitTests/Lab01Tests.cs
@@ -1,7 +1,61 @@
+using System;
+using AspNetCoreTest201908.Api.Lab01_ILogger;
+using AspNetCoreTest201908.Model;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging.Internal;
+using Xunit;
+
 namespace UnitTests
 {
     public class Lab01Tests
     {
+        [Fact]
+        public void Logger()
+        {
+            var lab01Controller = new Lab01Controller(new NullLogger<Lab01Controller>());
+
+            var result = lab01Controller.Index1() as OkObjectResult;
+            result.Value.As<AuthResult>().IsAuth.Should().BeTrue();
+        } 
         
+        [Fact]
+        public void Logger2()
+        {
+            var testLogger = new TestLogger<Lab01Controller>();
+            var lab01Controller = new Lab01Controller(testLogger);
+
+            var result = lab01Controller.Index1() as OkObjectResult;
+            result.Value.As<AuthResult>().IsAuth.Should().BeTrue();
+
+            testLogger.Get()[0].Value.Should().Be("log information");
+        } 
+    }
+
+    public class TestLogger<T> : ILogger<T>
+    {
+        private FormattedLogValues _formattedLogValues;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            _formattedLogValues = state as FormattedLogValues;
+        }
+
+        public FormattedLogValues Get()
+        {
+            return _formattedLogValues;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
