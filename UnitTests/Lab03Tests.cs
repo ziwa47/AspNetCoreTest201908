3b2719dd8b16fe1279fedf653c727c9b0e01f9b2
Cash <cash@cashwu.com>
2019-08-19 09:47:12 +0800

    Lab03 test

--- UnitTests/Lab03Tests.cs
+++ UnitTests/Lab03Tests.cs
@@ -1,7 +1,41 @@
+using AspNetCoreTest201908.Api.Lab03_IHostEnvironment;
+using AspNetCoreTest201908.Model;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
 namespace UnitTests
 {
     public class Lab03Tests
     {
+        [Fact]
+        public void Host1()
+        {
+            IHostingEnvironment environment = new HostingEnvironment
+            {
+                EnvironmentName = "Production"
+            };
+            var lab03Controller = new Lab03Controller(environment);
+
+            var result = lab03Controller.Index1() as OkObjectResult;
+
+            result.Value.As<EnvResult>().Env.Should().Be("Dev");
+        }        
         
+        [Fact]
+        public void Host2()
+        {
+            IHostingEnvironment environment = new HostingEnvironment
+            {
+                EnvironmentName = "Dev"
+            };
+            var lab03Controller = new Lab03Controller(environment);
+
+            var result = lab03Controller.Index1() as OkObjectResult;
+
+            result.Value.As<EnvResult>().Env.Should().Be("Prod");
+        }        
     }
 }
