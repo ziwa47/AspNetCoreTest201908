2a40b59c834a239e09f1869151ca7c29fb5b2dfc
Cash <cash@cashwu.com>
2019-08-19 09:13:34 +0800

    Lab00 test

--- UnitTests/Lab00Tests.cs
+++ UnitTests/Lab00Tests.cs
@@ -1,7 +1,19 @@
+using AspNetCoreTest201908.Api.Lab00;
+using AspNetCoreTest201908.Model;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
 namespace UnitTests
 {
     public class Lab00Tests
     {
-        
+        [Fact]
+        public void Test()
+        {
+            var lab00Controller = new Lab00Controller();
+            var result = lab00Controller.Index() as OkObjectResult;
+            result.Value.As<AuthResult>().IsAuth.Should().BeTrue();
+        }
     }
 }
