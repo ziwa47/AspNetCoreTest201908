708fdc58fdf32c1abe774c8ab966516ea645f7ec
阿輝 杜 <s3004856@hotmail.com>
2019-08-19 15:29:44 +0800

    auth test

--- UnitTests/Lab04Tests.cs
+++ UnitTests/Lab04Tests.cs
@@ -1,7 +1,63 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
+using AspNetCoreTest201908.Api.Lab02_IConfiguration;
+using AspNetCoreTest201908.Api.Lab04_HttpContext;
+using AspNetCoreTest201908.Model;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
 namespace UnitTests
 {
     public class Lab04Tests
     {
-        
+        [Fact]
+        public void Test11()
+        {
+            var httpContextAccessor = new HttpContextAccessor();
+            httpContextAccessor.HttpContext = new DefaultHttpContext()
+            {
+                User = new ClaimsPrincipal(new GenericIdentity("User")),
+            };
+            var lab04Controller = new Lab04Controller(httpContextAccessor);
+            var result = lab04Controller.Index1() as OkObjectResult;
+            result.Value.As<AuthResult>().IsAuth.Should().BeTrue();
+        }
+        [Fact]
+        public void Test12()
+        {
+            var httpContextAccessor = new HttpContextAccessor();
+            httpContextAccessor.HttpContext = new DefaultHttpContext()
+            {
+            };
+            var lab04Controller = new Lab04Controller(httpContextAccessor);
+            var result = lab04Controller.Index1() as OkObjectResult;
+            result.Value.As<AuthResult>().IsAuth.Should().BeFalse();
+        }
+        [Fact]
+        public void Test21()
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email,"abc@a.com"),
+                new Claim("MyType","myType"),
+            };
+
+            var httpContextAccessor = new HttpContextAccessor();
+            ClaimsIdentity identities = new ClaimsIdentity(claims);
+            httpContextAccessor.HttpContext = new DefaultHttpContext()
+            {
+                User = new ClaimsPrincipal(identities),
+            };
+            var lab04Controller = new Lab04Controller(httpContextAccessor);
+            var result = lab04Controller.Index2() as OkObjectResult;
+            result.Value.As<AuthClaim>().Email.Should().Be("abc@a.com");
+            result.Value.As<AuthClaim>().MyType.Should().Be("myType");
+
+        }
+
     }
 }
