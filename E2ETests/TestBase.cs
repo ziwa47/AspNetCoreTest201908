8eb386f546d5ba42d699ae31c2e417ee78e502b3
阿輝 杜 <s3004856@hotmail.com>
2019-08-27 14:15:15 +0800

    add 自訂義 host clinet

--- E2ETests/TestBase.cs
+++ E2ETests/TestBase.cs
@@ -1,6 +1,9 @@
 using System.Net.Http;
 using AspNetCoreTest201908;
+using AspNetCoreTest201908.Entity;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace E2ETests
@@ -8,15 +11,28 @@ namespace E2ETests
     public class TestBase : IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly WebApplicationFactory<Startup> _factory;
+        protected HttpClient appWebHost;
 
-        public TestBase(WebApplicationFactory<Startup> factory)
+        protected TestBase(WebApplicationFactory<Startup> factory)
         {
             _factory = factory;
         }
 
-        public HttpClient CreateHttpClient()
+        protected HttpClient CreateHttpClient()
         {
-            return _factory.CreateClient();
+            appWebHost = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(configureServices =>
+                {
+                    configureServices.AddDbContext<AppDbContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("memory");
+                    });
+                });
+            }).CreateClient();
+            return appWebHost;
+
+            //return _factory.CreateClient();
         }
     }
 }
